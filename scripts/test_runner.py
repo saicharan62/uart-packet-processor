8b4055dbece308a26b703b98e3ae9d835f34506d
sc-systems <saicharan.k.systems@gmail.com>
2026-03-28 19:03:47 +0530

    final testing script

--- scripts/test_runner.py
+++ scripts/test_runner.py
@@ -2,3 +2,4 @@ import os
 
 os.system("python3 scripts/packet_generator.py data/sample_packets.txt 20")
 os.system("./processor data/sample_packets.txt 80")
+os.system("python3 scripts/validator.py data/sample_packets.txt 80")
